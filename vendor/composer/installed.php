c66fcf03565bd3cde7f78601570d57ff2cdf0d48
Justin Kruit <justinkruitnl@gmail.com>
2021-06-18 14:39:23 +0200

    Upload vendor for GitHub updates

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '98f93dccaa2799fc8d4089c95b1245f2771a9a0b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '98f93dccaa2799fc8d4089c95b1245f2771a9a0b',
+            'dev_requirement' => false,
+        ),
+        'tracy/tracy' => array(
+            'pretty_version' => 'v2.8.5',
+            'version' => '2.8.5.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tracy/tracy',
+            'aliases' => array(),
+            'reference' => 'b23da41363546aa750d9f698f21e14815bd78913',
+            'dev_requirement' => false,
+        ),
+    ),
+);
