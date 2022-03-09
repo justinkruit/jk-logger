3d372768bcc5f2c4d6de67c8b15624525aadbbd2
Justin Kruit <justinkruitnl@gmail.com>
2022-03-09 16:48:16 +0100

    Move Dir and use proper Monolog functions

--- jk-debug.php
+++ jk-debug.php
@@ -97,40 +97,40 @@ class Log {
 	 */
 	protected static function configureInstance()
 	{
-		$dir = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'log';
+		$dir = wp_upload_dir()['basedir'] . '/log';
 
 		if (!file_exists($dir)){
 			mkdir($dir, 0777, true);
 		}
 
 		$logger = new Logger('RdLotteryManager');
-		$logger->pushHandler(new RotatingFileHandler($dir . DIRECTORY_SEPARATOR . 'main.log', 5));
+		$logger->pushHandler(new RotatingFileHandler($dir . '/main.log', 5));
 		//$logger->pushHandler(new LogglyHandler('eeb5ba83-f0d6-4273-bb1d-523231583855/tag/monolog'));
 		self::$instance = $logger;
 	}
 
 	public static function debug($message, array $context = []){
-		self::getLogger()->addDebug($message, $context);
+		self::getLogger()->debug($message, $context);
 	}
 
 	public static function info($message, array $context = []){
-		self::getLogger()->addInfo($message, $context);
+		self::getLogger()->info($message, $context);
 	}
 
 	public static function notice($message, array $context = []){
-		self::getLogger()->addNotice($message, $context);
+		self::getLogger()->notice($message, $context);
 	}
 
 	public static function warning($message, array $context = []){
-		self::getLogger()->addWarning($message, $context);
+		self::getLogger()->warning($message, $context);
 	}
 
 	public static function error($message, array $context = []){
-		self::getLogger()->addError($message, $context);
+		self::getLogger()->error($message, $context);
 	}
 
 	public static function critical($message, array $context = []){
-		self::getLogger()->addCritical($message, $context);
+		self::getLogger()->critical($message, $context);
 	}
 
 	public static function alert($message, array $context = []){
@@ -138,7 +138,7 @@ class Log {
 	}
 
 	public static function emergency($message, array $context = []){
-		self::getLogger()->addEmergency($message, $context);
+		self::getLogger()->emergency($message, $context);
 	}
 
 }
