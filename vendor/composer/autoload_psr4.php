2a55bc5916532f55b0f3d213ec1c2868ce5c6b48
Justin Kruit <justinkruitnl@gmail.com>
2022-03-09 16:15:49 +0100

    Add Monolog

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Psr\\Log\\' => array($vendorDir . '/psr/log/src'),
+    'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,28 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitf7aa97bfa316bc91b0ef0e62c163649a
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Psr\\Log\\' => 8,
+        ),
+        'M' => 
+        array (
+            'Monolog\\' => 8,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Psr\\Log\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/log/src',
+        ),
+        'Monolog\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/monolog/monolog/src/Monolog',
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
@@ -13,6 +35,8 @@ class ComposerStaticInitf7aa97bfa316bc91b0ef0e62c163649a
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInitf7aa97bfa316bc91b0ef0e62c163649a::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitf7aa97bfa316bc91b0ef0e62c163649a::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInitf7aa97bfa316bc91b0ef0e62c163649a::$classMap;
 
         }, null, ClassLoader::class);
